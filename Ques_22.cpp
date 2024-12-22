c407ed41f27883c628cbaa048a3112709e46befd
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-12-22 16:08:11 +0530

    Add files via upload

---
+++ Ques_22.cpp
@@ -0,0 +1,47 @@
+/*Given a linked list of 'N' nodes, where each node has an integer value that can be 0, 1, or 2.
+You need to sort the linked list in non-decreasing order and the return the head of the sorted list.*/
+
+Node *sortList(Node *head)
+{
+
+    int zeroCount = 0;
+    int oneCount = 0;
+    int twoCount = 0;
+
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == 0)
+            zeroCount++;
+        else if (temp->data == 1)
+            oneCount++;
+        else if (temp->data == 2)
+            twoCount++;
+
+        temp = temp->next;
+    }
+
+    temp = head;
+    while (temp != NULL)
+    {
+        if (zeroCount != 0)
+        {
+            temp->data = 0;
+            zeroCount--;
+        }
+        else if (oneCount != 0)
+        {
+            temp->data = 1;
+            oneCount--;
+        }
+        else if (twoCount != 0)
+        {
+            temp->data = 2;
+            twoCount--;
+        }
+
+        temp = temp->next;
+    }
+
+    return head;
+}
